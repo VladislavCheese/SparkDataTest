e30829f1352d7e76867bd152c09c500a8fe30ee9
vsyrov <89851502703l@gmail.com>
2022-01-16 20:30:27 +0300

    Добавлена ленивая коллекция

--- src/main/java/com/petproject/sparkdatatest/Order.java
+++ src/main/java/com/petproject/sparkdatatest/Order.java
@@ -4,25 +4,16 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import unsafe.starter.spark.data.annotations.ForeignKeyName;
 import unsafe.starter.spark.data.annotations.Source;
-
-import java.util.List;
-
 
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-@Source("data/criminals.csv")
-public class Criminal {
-
-    private long id;
-
+@Source("data/orders.csv")
+public class Order {
     private String name;
-
-    private int number;
-
-    @ForeignKeyName(fieldName = "criminalId")
-    private List<Order> orders;
+    private String desc;
+    private Integer pric;
+    private Long criminalId;
 }
